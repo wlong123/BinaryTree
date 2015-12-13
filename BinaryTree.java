6b7f8aefea14805fa84f474e49e88062de2f0d6e
wlong2017 <wlong123>
2015-12-12 21:24:17 -0500

    need to do toString, iterators, isBalanced, and isComplete

---
+++ BinaryTree.java
@@ -0,0 +1,147 @@
+public class BinaryTree<E>
+{
+	protected E value;
+	protected BinaryTree<E> left;
+	protected BinaryTree<E> right;
+	
+	public BinaryTree(E v, BinaryTree<E> l, BinaryTree<E> r)
+	{
+		value = v;
+		left = l;
+		right = r;
+	}
+	
+	public BinaryTree(E v)
+	{
+		value = v;
+		left = null;
+		right = null;
+	}
+	
+	public BinaryTree()
+	{
+		value = null;
+		left = null;
+		right = null;
+	}
+	
+	public BinaryTree<E> left()
+	{
+		return left;
+	}
+	
+	public BinaryTree<E> right()
+	{
+		return right;
+	}
+	
+	public E value()
+	{
+		return value;
+	}
+	
+	public void setLeft(BinaryTree<E> node)
+	{
+		left = node;
+	}
+	
+	public void setRight(BinaryTree<E> node)
+	{
+		right = node;
+	}
+	
+	public void setValue(E val)
+	{
+		value = val;
+	}
+	
+	public boolean isLeaf()
+	{
+		if((left == null) && (right == null))
+			return true;
+		return false;
+	}
+	
+	public int size()
+	{
+		if(isLeaf())
+			return 1;
+		if((left != null) && (right != null))
+			return 1 + left().size() + right.size();
+		if(right != null)
+			return 1 + right.size();
+		if(left != null)
+			return 1 + left.size();
+		return 0;
+	}
+/**
+	//public Iterator<E> preorderIterator()
+		//root
+		//left
+		//right
+	public Iterator<E> inorderIterator()
+	{
+		//left
+		//root
+		//right
+		
+	}
+	//public Iterator<E> postorderIterator()
+		//left
+		//right
+		//root
+*/	
+	public String toString()
+	{
+		String s = "";
+		if(value == null)
+			return "";
+		if((left != null) && (right != null))
+		{
+			if(s.contains(value.toString()))
+				s += "\n" + left().value() + "  " + right().value() + left.toString() + right.toString();
+			else
+			{
+				s += value.toString() + "\n" + left().value() + "  " + right().value() + left.toString() + right.toString();
+			}
+		}
+		else if(left != null)
+			if(s.contains(value.toString()))
+				s += "\n" + left().value() + left.toString();
+			else
+			{
+				s += value.toString() + "\n" + left().value() + left.toString();
+			}
+		else if(right != null)
+			if(s.contains(value.toString()))
+				s += "\n" + right().value() + right.toString();
+			else
+			{
+				s += value.toString() + "\n" + right().value() + right.toString();
+			}
+		return s;
+	}
+
+	public int height()
+	{
+		if(isLeaf())
+			return 0;
+		if((left != null) && (right != null))
+			return 1 + left().height() + right.height();
+		if(right != null)
+			return 1 + right.height();
+		if(left != null)
+			return 1 + left.height();
+		return 0;
+	}
+	
+	public boolean isFull()
+	{
+		if(isLeaf())
+			return true;
+		if((left == null) || (right == null))
+			return false;
+		return (left.isFull()) && (right().isFull()) && (right.height() == left.height());
+	}
+	
+}
---
+++ BinaryTreeRunner.java
@@ -0,0 +1,23 @@
+public class BinaryTreeRunner
+{
+	public static void main(String [] args)
+	{
+		BinaryTree<Integer> tree1 = new BinaryTree<Integer>(5);
+		tree1.setLeft(new BinaryTree<Integer>(4));
+		tree1.setRight(new BinaryTree<Integer>(3));
+		tree1.left().setLeft(new BinaryTree<Integer>(2));
+		tree1.left().setRight(new BinaryTree<Integer>(1));
+		/**
+			5
+		   / \
+		  4   3
+		 / \
+		2 	1*/
+		System.out.println(tree1.size());
+		System.out.println(tree1.height());
+		System.out.println(tree1.isFull());
+		System.out.println(tree1.left().isFull());
+		System.out.println(tree1.left().right().isFull());
+		System.out.println(tree1.toString());
+	}
+}
